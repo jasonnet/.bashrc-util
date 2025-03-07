3f169a635b7451cd6b0003f8404cb340510f251c
Jason Crawford <nnd15@Dell5515>
2025-03-07 12:20:09 -0500

    finish the transition to new directory name

--- create-bashrc.d.sh
+++ create-bashrc.d.sh
@@ -2,18 +2,18 @@
 
 mkdir -p ~/.bashrc.d
 
-export SLINK="jasonnet-bash-util.d"
+export SLINK="jasonnet-bashrc-util.d"
 if [ -h ~/.bashrc.d/$SLINK ] ; then
   #echo "exists"
   : # already done
 else 
   #echo "not exists"
   cd ~/.bashrc.d/
-  ln -s ~/.jasonnet-bash-util/bashrc/   $SLINK
+  ln -s ~/.bashrc-util/bashrc/   $SLINK
 fi
 
 
-THECMD="source ~/.jasonnet-bash-util/include-.bashrc.d-tree.source"
+THECMD="source ~/.bashrc-util/include-.bashrc.d-tree.source"
 grep "$THECMD" ~/.bashrc
 if [ "$?" != "0" ] ; then
   echo "you'll need to add the following (or equiv) to your .bashrc file:"
